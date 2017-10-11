db5434063fa228b4bf6efe5da2968920adf5a226
Kalpak Take <kalpaktake@gmail.com>
2017-10-11 11:42:10 +0000

    comments example

---
+++ syntax/Comments.java
@@ -0,0 +1,22 @@
+
+// Single line comments are really helpful
+/*
+    Multi-line comments are big!
+*/
+
+class Comments{
+    public static void main(String[] args) {
+        // Printing text to screen
+        System.out.println("I love comments!");
+    }
+}
+
+/**
+ * Local Documentation
+*/
+
+/*********
+    Creating HTML docs using javadoc which 
+    comes with JDK!
+    Looks good!
+*********/
