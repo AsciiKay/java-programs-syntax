b8e080b5b931422d9dc07e766359423ac8349f34
Kalpak Take <kalpaktake@gmail.com>
2017-10-12 21:41:50 +0530

    outputting strings

---
+++ syntax/Output_Text.java
@@ -0,0 +1,12 @@
+
+// Outputting Strings
+// printing text to screen/console
+
+class Output_Text {
+  public static void main(String[] args) {
+    System.out.println("Hello, World!");
+    System.out.println("I'm on console!");
+    int x = 144 + 169;
+    System.out.println("144 + 169 = " + x);
+  }
+}
