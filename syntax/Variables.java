27a35db521da49b4b185c515576f97590ec41367
Kalpak Take <kalpaktake@gmail.com>
2017-10-12 22:38:51 +0530

    variables and datatypes

---
+++ syntax/Variables.java
@@ -0,0 +1,23 @@
+// Variables-Datatypes
+// Int : Integers
+// String : Arr of chars 
+// Double : Decimal point numbers
+// Char : Alpha Numeric characters
+// Bool : Boolean values of true and/or false
+
+class Variables {
+    public static void main(String[] args) {
+        int calc = 1440 + 1550;  // value of calc set to integer 1440 + 1550
+        double pi = 3.14159;    // value of pi set double value of 'pi'
+        String lang = "Java";   // Value of lang set to string "Java"
+        char initial = 'K';    // value of initial set to char value of 'K'
+        boolean truth = true;   // value of truth set to bool value of true 
+        int big = 8700 * 7700;  // value of big set to really big integer value
+        
+        System.out.println("1440 + 1550" + calc);
+        System.out.println("Value of pi: " + pi);
+        System.out.println(lang + "'s syntax is c++ alike!");
+        System.out.println(initial + " is the first letter in my name!");
+        System.out.println(big + " is a really big number!");
+    }
+}
