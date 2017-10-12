e9107065174346b64f6c67e1f69b60130a8ad24f
Kalpak Take <kalpaktake@gmail.com>
2017-10-12 23:00:44 +0530

    math operations

---
+++ syntax/Math.java
@@ -0,0 +1,24 @@
+// Math operators
+// + : addition
+// - : subtraction
+// * : multiplication
+// / : division
+// % : modulo
+// 
+// For both ints and doubles
+
+class Math {
+    public static void main(String[] args) {
+        int sum = 9 + 9 + 1;
+        double subs = 99.314159 - 0.314159;
+        int mult = 89 * 89;
+        double div = 11 / 3;
+        int mod = 17 % 5;
+        
+        System.out.println("9 + 9 + 1 = " + sum);
+        System.out.println("99.314159 - 0.314159 = " + subs);
+        System.out.println("89 * 89 = " + mult);
+        System.out.println("11 / 3 = " + div);
+        System.out.println("17 % 5 = " + mod);
+    }
+}
