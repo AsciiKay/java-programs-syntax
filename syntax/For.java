8ab629c6bd8d9de13593502d9587e7ef6a2734f0
Kalpak Take <kalpaktake@gmail.com>
2017-10-13 21:45:27 +0530

    for loops

---
+++ syntax/For.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+// For loop 
+// for loop is like while loop but has a definite end point 
+// while loops are abstract in nature and can be infinte 
+// for loops cannot be infinite
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner input_end = new Scanner(System.in);
+    System.out.println("Enter a number:");
+    int end = input_end.nextInt();  // End point for print squares till that number 
+    
+    // printing squares of all numbers till end var 
+    for (int x = 1; x <= end; x++) {
+      int sq = x * x;   // squaring 
+      System.out.println("Square of " + x + " : " + sq);
+    }
+    
+    Scanner input_tab = new Scanner(System.in);
+    System.out.println("\nEnter a number:");
+    int tab = input_tab.nextInt();   // number for printing its multiplication table 
+    
+    // Printing multiplication table of tab till 12 
+    for (int i = 0; i <= 12; ++i) {
+      int tmp = tab * i;
+      System.out.println(i + " times " + tab + " is " + tmp);
+    }
+    
+  }
+}
